10436ec070796c06d5cb9cd5b4e3898ec6bf0086
a1p4ca <dreamaker7770@gmail.com>
2019-11-27 11:06:58 +0900

    feat: implementation

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from sds011 import SDS011
+from adafruit_dht import DHT22
+import board
+import os
+
+dust_sensor = SDS011(os.environ['SDS011_SERIAL_PORT'])
+dht = DHT22(board.D18)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def status():
+  dust = dust_sensor.query()
+  return jsonify({
+    'temperature': dht.temperature,
+    'humidity': dht.humidity,
+    'pm10': dust[0],
+    'pm2.5': dust[1]
+  })
+
+if __name__ == '__main__':
+  app.run(debug=True)
