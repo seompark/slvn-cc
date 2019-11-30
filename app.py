7a0914764eff449f4d275914d6bc0dc3ea77360d
a1p4ca <dreamaker7770@gmail.com>
2019-11-30 16:01:48 +0900

    asdf

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from sds011 import SDS011
-from adafruit_dht import DHT22
-import board
 import os
 import time
 
-dust_sensor = SDS011(os.environ['SDS011_SERIAL_PORT'])
-dht = DHT22(board.D18)
+# dust_sensor = SDS011('COM7')
 
 app = Flask(__name__)
-dust_sensor.sleep(sleep=False)
+# dust_sensor.sleep(sleep=False)
 
-def get_dht_data():
-    while True:
-        try:
-            temperature, humidity = dht.temperature, dht.humidity
-            if temperature is not None and humidity is not None:
-                return temperature, humidity
-            else:
-                raise
-        except:
-            time.sleep(0.5)
 
-@app.route('/', methods=['GET'])
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+@app.route('/api', methods=['GET'])
 def status():
-    dust = dust_sensor.query()
-    temperature, humidity = get_dht_data()
-
+   # dust = dust_sensor.query()
+    dust = ['45', '59']
     return jsonify({
-        'temperature': dht.temperature,
-        'humidity': dht.humidity,
         'pm10': dust[0],
-        'pm2.5': dust[1]
+        'pm25': dust[1]
     })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
